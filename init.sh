5407f9fc85cc32c745439e50b8cf468644c8c8b4
Mhd Wissam AlRoujoulah <w.roujoulah@gmail.com>
2020-11-17 16:24:44 +0800

    Support helm3 & installing cert-manager & change the secret type to TLS

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-helm init &&
+helm repo add jetstack https://charts.jetstack.io && \
+  helm repo update && \
+  helm install cert-manager \
+    --namespace cert-manager \
+    --version v1.0.4  \
+    jetstack/cert-manager \
+    --set installCRDs=true \
+    --wait &&
 
 rm -rf tmp/ &&
 mkdir tmp/ &&
@@ -8,6 +15,6 @@ mkdir tmp/ &&
 openssl ecparam -name secp384r1 -genkey -noout -out tmp/ca.key &&
 openssl req -x509 -new -nodes -key tmp/ca.key -subj "/CN=hatch-ca" -days 3650 -out tmp/ca.crt &&
 
-kubectl create secret generic hatch-ca --from-file=tmp/ca.key --from-file=tmp/ca.crt
+kubectl create secret tls hatch-ca --cert=tmp/ca.crt  --key=tmp/ca.key
 
 rm -rf tmp/
